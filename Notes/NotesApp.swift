38c6532ab22317861449255ba204458cb9518538
Nick Rizk <nrizk6@gmail.com>
2026-03-16 10:42:34 -0400

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Nick Rizk on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
